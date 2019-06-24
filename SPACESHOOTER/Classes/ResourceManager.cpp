418d5f7ee80236e289bb9d173d847d3f2825d075
nguyentantai15t3 <taijoke97@gmail.com>
2019-06-24 16:17:33 +0700

    + add code func Load() in ResourceManager.cpp

--- SPACESHOOTER/Classes/ResourceManager.cpp
+++ SPACESHOOTER/Classes/ResourceManager.cpp
@@ -1,7 +1,13 @@
 #include "ResourceManager.h"
+#include <fstream>
+#include <iostream>
+
+USING_NS_CC;
+using namespace std;
 
 ResourceManager::ResourceManager()
 {
+	this->Init("res");
 }
 
 ResourceManager::~ResourceManager()
@@ -10,11 +16,74 @@ ResourceManager::~ResourceManager()
 
 void ResourceManager::Init(const string path)
 {
+	this->m_dataFolderPath = path;
+	this->Load(this->m_dataFolderPath + "\Data.bin");
 }
 
 void ResourceManager::Load(string fileName)
 {
-
+	ifstream inFile;
+	inFile.open(fileName);
+	if (inFile.is_open())
+	{
+		while (!inFile.eof())
+		{
+			string temp;
+			string normal_path;
+			string pressed_path;
+			int size;
+			char id;
+			inFile >> temp;
+			if (temp == "#SPRITE")
+			{
+				inFile >> size;
+				for (int i = 0; i < size; i++);
+				{
+					inFile >> temp;
+					inFile >> id;
+					inFile >> temp;
+					inFile >> normal_path;
+					normal_path.replace(normal_path.find_first_of("%s"), 2, this->m_dataFolderPath);
+					auto sprite = Sprite::create(normal_path);
+					this->m_sprites.insert(pair<char, Sprite*>(id, sprite));
+				}
+			}
+			else if (temp == "#BUTTON")
+			{
+				inFile >> size;
+				for (int i = 0; i < size; i++);
+				{
+					inFile >> temp;
+					inFile >> id;
+					inFile >> temp;
+					inFile >> normal_path;
+					inFile >> temp;
+					inFile >> pressed_path;
+					normal_path.replace(normal_path.find_first_of("%s"), 2, this->m_dataFolderPath);
+					auto button = ui::Button::create(normal_path, pressed_path);
+					this->m_button.insert(pair<char, ui::Button*>(id, button));
+				}
+			}
+			else
+			{
+				inFile >> size;
+				for (int i = 0; i < size; i++);
+				{
+					inFile >> temp;
+					inFile >> id;
+					inFile >> temp;
+					inFile >> normal_path;
+					normal_path.replace(normal_path.find_first_of("%s"), 2, this->m_dataFolderPath);
+					auto lable = Label::createWithTTF(normal_path,"");
+					this->m_lable.insert(pair<char, Label*>(id, lable));
+				}
+			}
+		}
+	}
+	else
+	{
+		cout << "Load file error" << endl;
+	}
 }
 
 Sprite * ResourceManager::GetSpriteById(char id)
